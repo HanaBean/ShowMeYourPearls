1c627a8dbc4286b08b6ecbd7b794fe3d740b6118
DS <DS@603-10>
2021-04-23 09:31:32 +0900

    version0

---
+++ src/main/java/com/pearl/SmypApplication.java
@@ -0,0 +1,13 @@
+package com.pearl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmypApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmypApplication.class, args);
+	}
+
+}
