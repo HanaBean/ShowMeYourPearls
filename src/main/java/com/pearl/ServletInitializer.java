a2ff27fe1f56e7dd99d0bfa7007e70b2876eebdc
DS <DS@603-02>
2021-04-23 10:31:17 +0900

    version0

--- src/main/java/com/pearl/ServletInitializer.java
+++ src/main/java/com/pearl/ServletInitializer.java
@@ -3,6 +3,9 @@ package com.pearl;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import lombok.Data;
+import lombok.extern.log4j.Log4j;
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
