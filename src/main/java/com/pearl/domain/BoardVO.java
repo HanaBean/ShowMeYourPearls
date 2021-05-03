ddaefd23508a0dd9b819b46133db87b6339ef7f9
DS <DS@603-10>
2021-05-03 09:25:51 +0900

    .

--- src/main/java/com/pearl/domain/BoardVO.java
+++ src/main/java/com/pearl/domain/BoardVO.java
@@ -1,13 +1,15 @@
 package com.pearl.domain;
 
-import java.util.Date;
+import java.sql.Date;
 
 import lombok.Data;
 
 @Data
 public class BoardVO {
-	private Long boardNum,memNum;
-	private String boardTitle, boardContent;
+	private Long boardNum;
+	private Long memNum;
+	private String boardTitle;
+	private String boardContent;
 	private Date boardDate;
-	private char boardType;
+	private String boardType;
 }
--- src/main/java/com/pearl/mapper/MemberMapper.java
+++ src/main/java/com/pearl/mapper/MemberMapper.java
@@ -9,9 +9,9 @@ import com.pearl.domain.MemberVO;
 @Mapper
 public interface MemberMapper {
 	List<MemberVO> list();
-	MemberVO get(Long memNum);
-	Long insert(MemberVO vo);
+	MemberVO get(int memNum);
+	int insert(MemberVO vo);
 	int update(MemberVO vo);
 	int updateLevel(MemberVO vo);
-	int delete(Long memNum);
+	int delete(int memNum);
 }
