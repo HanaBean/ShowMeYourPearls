35dd0ca795303595fc6d9a430d6cb8698f53da69
DS <DS@603-04>
2021-05-04 09:56:14 +0900

    합★체 공지사항

--- src/main/java/com/pearl/controller/NoticeController.java
+++ src/main/java/com/pearl/controller/NoticeController.java
@@ -3,8 +3,12 @@ package com.pearl.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.pearl.domain.BoardVO;
+import com.pearl.service.NoticeService;
 
 import lombok.Setter;
 
@@ -12,9 +16,58 @@ import lombok.Setter;
 @RequestMapping("/notice/*")
 public class NoticeController {
 	
+	@Setter(onMethod_ = @Autowired)
+	private NoticeService service;
+	
 	@RequestMapping("/list")
-	public ModelAndView list() {
+	public ModelAndView list(Model model) {
 		ModelAndView mv = new ModelAndView("notice/list");
+		mv.addObject("list", service.list());
+		
+		return mv;
+	}
+
+	@RequestMapping("/read")
+	public ModelAndView read(int boardNum, Model model) {
+		ModelAndView mv = new ModelAndView("notice/read");
+		mv.addObject("notice", service.read(boardNum));
+		
+		return mv;
+	}
+	
+	@RequestMapping("/write")
+	public ModelAndView writePage() {
+		ModelAndView mv = new ModelAndView("notice/write");
+		
+		return mv;
+	}
+	
+	@PostMapping("/write")
+	public ModelAndView writeSend(BoardVO vo) {
+		ModelAndView mv = new ModelAndView("redirect:/notice/list");
+		service.insert(vo);
+		return mv;
+	}
+	
+	@RequestMapping("/modify")
+	public ModelAndView modifyPage(int boardNum, Model model) {
+		ModelAndView mv = new ModelAndView("notice/modify");
+		mv.addObject("notice", service.read(boardNum));
+		
+		return mv;
+	}
+	
+	@PostMapping("/modify")
+	public ModelAndView modifySend(BoardVO vo) {
+		ModelAndView mv = new ModelAndView("redirect:/notice/list");
+		service.update(vo);
+		return mv;
+	}
+	
+	@RequestMapping("/delete")
+	public ModelAndView delete(int boardNum, Model model) {
+		ModelAndView mv = new ModelAndView("redirect:/notice/list");
+		service.delete(boardNum);
 		
 		return mv;
 	}
