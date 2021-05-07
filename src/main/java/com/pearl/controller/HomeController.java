19b2434395bd3c0e53b81efd082023adaf39792a
DS <DS@603-09>
2021-05-07 12:44:17 +0900

    SEMI

--- src/main/java/com/pearl/controller/HomeController.java
+++ src/main/java/com/pearl/controller/HomeController.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/home/*")
+
 public class HomeController {
 
-	@RequestMapping("/main")
+	@RequestMapping("/")
 	public ModelAndView main() {
 		ModelAndView mv = new ModelAndView("home/main");
 		return mv;
 	}
 	
-
+	
 }
