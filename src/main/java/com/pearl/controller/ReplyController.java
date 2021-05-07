03ba0f99dd86e5455066703ab2a578b97bdec649
DS <DS@603-10>
2021-05-07 15:57:18 +0900

    reply CRUD 끝

--- src/main/java/com/pearl/controller/ReplyController.java
+++ src/main/java/com/pearl/controller/ReplyController.java
@@ -6,12 +6,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.pearl.domain.ReplyVO;
@@ -44,6 +47,14 @@ public class ReplyController {
 	public ResponseEntity<String> delete(@RequestParam("replyNum") int replyNum) {
 		log.info("Delete>>>>>>>>"+replyNum);
 		service.delete(replyNum);
+		return new ResponseEntity<String>("success", HttpStatus.OK);
+	}
+	
+	@PostMapping(value="/update", consumes = MediaType.APPLICATION_JSON_VALUE,
+	        produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> update(@RequestBody ReplyVO vo) {
+		log.info("Delete>>>>>>>>"+vo);
+		service.update(vo);
 		return new ResponseEntity<String>("success", HttpStatus.OK);
 	}
 	
