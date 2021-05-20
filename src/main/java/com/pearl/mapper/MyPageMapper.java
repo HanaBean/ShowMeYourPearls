871eed0fbdcd4de73ffe555e0d7eb9ce2ad179bf
DS <DS@603-10>
2021-05-20 14:21:27 +0900

    MDG -- 구독, MyGallery 완료

--- src/main/java/com/pearl/mapper/MyPageMapper.java
+++ src/main/java/com/pearl/mapper/MyPageMapper.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.pearl.domain.BoardVO;
+import com.pearl.domain.GalleryVO;
+import com.pearl.domain.MemberVO;
 import com.pearl.domain.MyPageVO;
 import com.pearl.domain.SubscribeVO;
 
@@ -12,7 +14,9 @@ import com.pearl.domain.SubscribeVO;
 public interface MyPageMapper {
 	List<MyPageVO> myfundList(Long memNum);
 	List<MyPageVO> donaList(Long memNum);
-	List<BoardVO> mygallery(Long memNum);
-	int subCount(Long memNum);
 	int subscribe(SubscribeVO subscribe);
+	int unsubscribe(SubscribeVO subscribe);
+	List<SubscribeVO> subList(Long memNum);
+	List<MemberVO> mySubList(Long memNum);
+	List<GalleryVO> myGallery(Long memNum);
 }
--- src/main/java/com/pearl/service/MyPageService.java
+++ src/main/java/com/pearl/service/MyPageService.java
@@ -3,6 +3,8 @@ package com.pearl.service;
 import java.util.List;
 
 import com.pearl.domain.BoardVO;
+import com.pearl.domain.GalleryVO;
+import com.pearl.domain.MemberVO;
 import com.pearl.domain.MyPageVO;
 import com.pearl.domain.SubscribeVO;
 
@@ -10,7 +12,9 @@ import com.pearl.domain.SubscribeVO;
 public interface MyPageService {
 	List<MyPageVO> donaList(Long memNum);
 	List<MyPageVO> myfundList(Long memNum);
-	List<BoardVO> mygallery(Long memNum);
 	int subscribe(SubscribeVO subscribe);
-	int subCount(Long memNum);
+	int unsubscribe(SubscribeVO subscribe);
+	List<SubscribeVO> subList(Long memNum);
+	List<MemberVO> mySubList(Long memNum);
+	List<GalleryVO> myGallery(Long memNum);
 }
