4bc829a593eed478e3fa04ff06d00a6d84f24d2f
DS <DS@603-10>
2021-05-21 16:35:59 +0900

    cc

--- src/main/java/com/pearl/controller/MypageController.java
+++ src/main/java/com/pearl/controller/MypageController.java
@@ -1,6 +1,10 @@
 package com.pearl.controller;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,10 +16,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.pearl.domain.CustomUser;
 import com.pearl.domain.MemberVO;
+import com.pearl.domain.PictureVO;
 import com.pearl.domain.SubscribeVO;
 import com.pearl.service.MemberService;
 import com.pearl.service.MyPageService;
@@ -78,7 +85,8 @@ public class MypageController {
 	
 	@PreAuthorize("principal.username == #vo.memEmail")
 	@PostMapping("/mypage/editsend")
-	 public String Editsend(MemberVO vo) {
+	 public String Editsend(MemberVO vo,@RequestParam("file") MultipartFile file) {
+		 if(file.getSize()>0) vo.setProfile(uploadPicture(file));
 		 user.updateUser(vo); 
 		 return "redirect:edit";
 	}
@@ -122,6 +130,47 @@ public class MypageController {
 		}
 		mypageservice.subscribe(subscriber);
 		return new ResponseEntity<Integer>(subLists.size()+1, HttpStatus.OK);
+	}
+
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String str = sdf.format(new Date());
+		return str.replace("-", File.separator); //separator:폴더와 폴더의 구분자
+	}
+	
+	private PictureVO uploadPicture(MultipartFile file) {
+		PictureVO picture = new PictureVO();
+		String uploadFolder = "c:\\pearl";
+		
+		//저장 경로를 File객체에 담음. 파일이 아닌 디렉토리
+		File uploadPath = new File(uploadFolder, getFolder());
+		log.info("uploadPath: "+uploadPath);
+		if(uploadPath.exists()==false) uploadPath.mkdirs();
+		
+		log.info("uploadFile Name :" + file.getOriginalFilename());
+		log.info("uploadFile Size :" + file.getSize());
+		
+		String uploadFileName = file.getOriginalFilename();
+		
+		//IE has file path
+		uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\")+1);
+		log.info(uploadFileName);
+		picture.setPicName(uploadFileName.substring(0, uploadFileName.lastIndexOf(".")));
+		picture.setPicTail(uploadFileName.substring(uploadFileName.lastIndexOf(".")+1));
+		
+		UUID uuid = UUID.randomUUID();
+		uploadFileName = uuid.toString()+"_"+uploadFileName;
+		
+		File saveFile = new File(uploadPath, uploadFileName);
+		try {
+			file.transferTo(saveFile);
+			picture.setPicUuid(uuid.toString());
+			picture.setPicPath(getFolder());
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		} //end catch
+		
+		return picture;
 	}
 	
 }
