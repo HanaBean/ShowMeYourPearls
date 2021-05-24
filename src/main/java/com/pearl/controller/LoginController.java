40e362f61b707226bb429eb1ec65fca9c15b0f56
DS <DS@603-10>
2021-05-24 17:13:15 +0900

    MDG -- Login/Join 유효성 검사

--- src/main/java/com/pearl/controller/LoginController.java
+++ src/main/java/com/pearl/controller/LoginController.java
@@ -2,16 +2,19 @@ package com.pearl.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.pearl.domain.MemberVO;
 import com.pearl.service.UserDetailServiceImpl;
 
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Slf4j
 @Controller
 public class LoginController { 
 	
@@ -19,11 +22,20 @@ public class LoginController {
 	private UserDetailServiceImpl user;
 	
 	@GetMapping("/login")
-	public void login() {}
+	public void login(String error, Model model) {
+		model.addAttribute("error",error);
+	}
 	
 	@PostMapping("/join")
-	public String insertForm(MemberVO vo) {
-		user.joinUser(vo);
+	public String insertForm(MemberVO vo, RedirectAttributes rttr) {
+		log.info("VO1>>>>>"+vo);
+		String email = vo.getMemEmail();
+		if(!user.emailCheck(email)) {
+			rttr.addFlashAttribute("result","joinFail");
+		}else {
+			log.info("VO2>>>>>"+vo);
+			user.joinUser(vo);
+		}
 		return "redirect:/login";
 	}
 	
